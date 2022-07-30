964af51e537546aeca96538d66a584d422f6c77f
soledademaria <solmmaria50@gmail.com>
2022-07-29 21:12:30 -0300

    modal utilizando html, css e js

---
+++ script.js
@@ -0,0 +1,17 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
+
+
+const showModal = () => {
+    [modal, fade].forEach((el) => el.classList.remove("hide"));
+}
